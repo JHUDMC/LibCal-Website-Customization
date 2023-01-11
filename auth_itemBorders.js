a459f6b25b172202539953311dfe8953d4170a57
pignoodle <mason.gareis@gmail.com>
2023-01-11 18:52:06 -0500

    Dynamically updates to page changes

--- auth_itemBorders.js
+++ auth_itemBorders.js
@@ -3,7 +3,9 @@ File: auth_itemBorders.js
 Purpose: Adds a border to items requiring authorization
 Author: Mason Gareis (mason.gareis@gmail.com)
 Date Created: 01/11/2023
+Notes: Known bug that doesn't add banner to every item on the page. (border is fine tho)
 */
+
 
 // Check for authed items on equipment pages only
 if(/^https:\/\/jhu-dmc\.libcal\.com\/equipment/.test(window.location.href)) {
@@ -22,41 +24,50 @@ if(/^https:\/\/jhu-dmc\.libcal\.com\/equipment/.test(window.location.href)) {
     ]; // list of all equipment IDs requiring authorization.
 
     // --------------------------------------STOP EDITING CODE HERE ---------------------------------------- //
-
-
-
     // Auth banner template element that will be inserted
     var authBanner = document.createElement("span");
     authBanner.classList.add("auth-banner");
     authBanner.innerHTML = "Auth Required*"; // Banner Text
+    
+    // Observe item cards for changes
+    const targetNode = document;
+    // create an observer instance to watch for changes
+    const observer = new MutationObserver(() => {
+        // code to be executed when the page updates
 
-    // Wait for page to fully load
-    window.onload = function() {
         // Get all item cards (class="col-item")
         let itemCards = document.getElementsByClassName("col-item");
         // Find items requiring authorization
         let pageAuthItems = filterAuthedItems(itemCards);
         for(let i = 0; i < pageAuthItems.length; i++) {
-            // Add border to card
-            pageAuthItems[i].classList.add("auth-item-card");
-            // Insert banner directly after the card's opening div tag
-            pageAuthItems[i].insertAdjacentElement("afterbegin", authBanner);
-        };
-    };
-
-    // Function to filter for authed items
-    function filterAuthedItems(itemCards) {
-        let pageAuthList = [];
-        for(let i = 0; i < itemCards.length; i++) {
-            // Get EID from photo link
-            let eid = itemCards[i].getElementsByClassName("btn-add")[0].getElementsByTagName("a")[0].getAttribute("data-eid");
-            // If in master auth list
-            if(masterAuthList.indexOf(eid) > -1) {
-                // Add item to authed items list
-                pageAuthList.push(itemCards[i]);
+            // Check for no exisiting auth border
+            if(!pageAuthItems[i].classList.contains("auth-item-card")) {
+                // Add border to card
+                pageAuthItems[i].classList.add("auth-item-card");
+                // Insert banner directly after the card's opening div tag
+                pageAuthItems[i].insertAdjacentElement("afterbegin", authBanner);
             };
         };
-        return pageAuthList;
-    };
+
+        // Function to filter for authed items
+        function filterAuthedItems(itemCards) {
+            let pageAuthList = [];
+            for(let i = 0; i < itemCards.length; i++) {
+                // Get EID from photo link
+                let eid = itemCards[i].getElementsByClassName("btn-add")[0].getElementsByTagName("a")[0].getAttribute("data-eid");
+                // If in master auth list
+                if(masterAuthList.indexOf(eid) > -1) {
+                    // Add item to authed items list
+                    pageAuthList.push(itemCards[i]);
+                };
+            };
+            return pageAuthList;
+        };
+    });
+
+    // configuration of the observer
+    const config = { childList: true, subtree: true };
+    // observe changes!
+    observer.observe(targetNode, config);
 
 };
