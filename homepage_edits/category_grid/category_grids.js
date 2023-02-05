376b9ac7a9d526aa88cb24e761f4f11d63d8800c
pignoodle <mason.gareis@gmail.com>
2023-02-05 15:48:54 -0500

    Changed some JS to CSS to make lightweight

--- homepage_edits/category_grid/category_grids.js
+++ homepage_edits/category_grid/category_grids.js
@@ -1,43 +1,21 @@
 // JS script for category grids
-// This is used to hide/show grid items & resize grid items to be square
+// This is used to hide/show grid items
 
 
-// Once html is fully loaded
-window.onload = function() {
-    // Resize grid items to be square aspect ratio or longer
-    resizeGridItems();
-};
-// Resize grid items to be square on window resize
-window.onresize = function() {
-    resizeGridItems();
-};
-
-// Function to resize grid items to be square
-var resizeGridItems = function() {
-    // Get all grid items
-    var gridItems = document.getElementsByClassName("grid-item");
-    // Loop through grid items
-    for (let i = 0; i < gridItems.length; i++) {
-        // Get grid item
-        let gridItem = gridItems[i];
-        // Reset height to auto
-        gridItem.style.height = "auto";
-        // Get height and width
-        let gridItemHeight = gridItem.clientHeight;
-        let gridItemWidth = gridItem.clientWidth;
-        // Resize if not at least 1:1 (i.e. block is too short)
-        if (gridItemHeight < gridItemWidth) {
-            gridItem.style.height = gridItemWidth + "px";
-        };
-    };
-};
-
-// Function to hide or show grid items
+// Function to hide or show grid items when the titlebar is clicked
 var hideShowGridItems = function(event) {
-    // get grid container of the current category
-    let gridContainer = event.target.parentNode.nextElementSibling;
-    // toggle grid container visibility
+    // Get event target class name
+    let catContainer = event.target.parentElement;
+    // Loop up to parent grid container
+    while (catContainer.className !== "category-container") {
+        catContainer = catContainer.parentElement;
+    };
+    // Get grid container
+    let gridContainer = catContainer.getElementsByClassName("grid-container")[0];
+    // Toggle grid container visibility
     gridContainer.style.display = gridContainer.style.display === "none" ? "grid" : "none";
-    // change hide button symbol
-    event.target.innerHTML = event.target.innerHTML === "▼" ? "▲" : "▼";
+    // Get hide/show button
+    let hideShowButton = catContainer.getElementsByClassName("titlebar-hideshowbutton")[0];
+    // Change hide button symbol
+    hideShowButton.innerHTML = hideShowButton.innerHTML === "▼" ? "▲" : "▼";
 };
