c3e225c3656058905cc5782bb0139e26dc189920
pignoodle <mason.gareis@gmail.com>
2023-02-12 16:30:30 -0500

    Updated comments

--- allPages_edits/staffLoginTextChange.js
+++ allPages_edits/staffLoginTextChange.js
@@ -1,12 +1,15 @@
 // Script to change the text "Login to LibApps" to "Staff Login"
 // in the footer of the LibApps public pages
 
-// Find admin footer
-let adminFooter = document.getElementById("s-lc-public-footer-admin-links");
-// Get Login to LibApps link
-let loginLink = adminFooter.getElementsByTagName("a")
-// Be extra sure we have the right link
-if (loginLink[0].id == "s-lc-sign-in") {
-    // Change the text
-    loginLink[0].innerHTML = "DMC Staff Login";
-}
+// on DOM load, run the function
+    document.addEventListener("DOMContentLoaded", function() {
+    // Find admin footer
+    let adminFooter = document.getElementById("s-lc-public-footer-admin-links");
+    // Get Login to LibApps link
+    let loginLink = adminFooter.getElementsByTagName("a")
+    // Be extra sure we have the right link
+    if (loginLink[0].id == "s-lc-sign-in") {
+        // Change the text
+        loginLink[0].innerHTML = "DMC Staff Login";
+    };
+});
